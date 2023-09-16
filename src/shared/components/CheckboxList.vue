619cec1c10fbd92b0efc4c1eead5f6c7f4e2d029
NyanRus <68762426+nyanrus@users.noreply.github.com>
2023-09-17 00:23:24 +0900

    wip

--- src/shared/components/CheckboxList.vue
+++ src/shared/components/CheckboxList.vue
@@ -3,7 +3,15 @@
         <summary class="r-jwli3a r-1tl8opc r-qvutc0 r-bcqeeo TUIC_setting_title">
             {{ TUICI18N.get(title) }}
         </summary>
-        <div class="TUIC_col_setting_container">{{ TUICInvisibleCheckBox }}</div>
+        <div class="TUIC_col_setting_container">
+            <checkbox
+                v-for="i in TUICData[id].all"
+                :id="i"
+                :value="settings[id][i]"
+                :name="TUICData[id].i18n[i]"
+                :type="type"
+            />
+        </div>
         <br />
         {{ otherSetting ?? "" }}
     </details>
@@ -11,33 +19,18 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import checkbox from "./checkbox.vue";
 
 import { TUICI18N } from "../../content/i18n";
+import { TUICData } from "../../content/data";
 
-// //チェックボックスリスト(id:ID title:Stringでタイトル)
-// checkboxList: function (id, title, type, otherSetting) {
-//         let TUICInvisibleCheckBox = "";
-//         for (const i of TUICData[id].all) {
-//             TUICInvisibleCheckBox += this.checkbox(i, settings[id][i], TUICData[id].i18n[i], type);
-//         }
-// return `
-// <details class="TUICDetails">
-//     <summary class="r-jwli3a r-1tl8opc r-qvutc0 r-bcqeeo TUIC_setting_title">${TUICI18N.get(
-//         title,
-//     )}</summary>
-//   <div class="TUIC_col_setting_container">
-//       ${TUICInvisibleCheckBox}
-//   </div>
-//   <br>
-//   ${otherSetting ?? ""}
-// </details>
-//           `;
-//     },
 export default defineComponent({
     setup() {
-        return { TUICI18N };
+        return { TUICI18N, TUICData };
     },
-    props: ["title", "otherSetting"],
+    // //チェックボックスリスト(id:ID title:Stringでタイトル)
+    props: ["title", "otherSetting", "id", "settings", "type"],
+    components: { checkbox },
 });
 </script>
 
